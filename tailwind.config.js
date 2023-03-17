0ec16a38e366bf63af492a012f05b34432a6061b
ahmadreza tahmasbi <ahmadreza.tmb@gmail.com>
2023-03-17 15:21:24 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary" : "#D91F27",
+        "secondary" : "#494b57",
+      }
+    },
+
+  },
+  plugins: [],
+}
